049a33f2b7742a008eb0c4c100885e82dcbbd5c2
Daniela Shmayev <danishmayev@gmail.com>
2020-12-29 16:17:25 +0000

    conditional change

--- product/js/input_display.js
+++ product/js/input_display.js
@@ -2,37 +2,63 @@ $(document).ready(function() {
 
     var fst = $(".first-descr");
     var furn = $(".second-descr, .third-descr");
-    var p = $('#descr-message');
-    
+
+    var optObj = {
+        'DVD-Disc': {  
+            txt: '"Please, provide the products dimensions in MB."',
+            lbl1: "Size (MB)",
+            display: {
+               show: $(".first-descr"),
+               noshow: $(".second-descr, .third-descr")
+            }
+        },
+        'Book': {
+            txt: '"Please, provide the products weight in KG."',
+            lbl1: "Weight (KG)",
+            display: {
+                show: $(".first-descr"),
+                noshow: $(".second-descr, .third-descr")
+            }
+        },
+        'Furniture': {
+            txt: '"Please, provide the products dimentions as HxWxL."',
+            lbl1: "Height (CM)",
+            display: {
+                show: $(".first-descr, .second-descr, .third-descr")
+            }
+        },
+        '' : {
+            txt: '',
+            lbl1: '',
+            display: {
+                noshow: $(".first-descr, .second-descr, .third-descr")
+            }
+        },
+    };
+
     fst.hide();
     furn.hide();
     
     $('#product_type').change(function() {
-        var type = $(this).val();
-        switch (type) {
-            case 'DVD-Disc':
-                fst.show();
-                furn.hide();
-                fst.find("label").text("Size (MB)");
-                p.text('"Please, provide the products dimensions in MB."');
-                break;
-            case 'Book':
-                fst.show();
-                furn.hide();
-                fst.find("label").text("Weight (KG)");
-                p.text('"Please, provide the products weight in KG."');
-                break;
-            case 'Furniture':
-                fst.show();
-                furn.show();
-                fst.find("label").text("Height (CM)");
-                p.text('"Please, provide the products dimentions as HxWxL."');
-                break;
-            default:
-                fst.hide();
-                furn.hide();
-                p.empty();
-        }
+        $("#product_type").each(function() {
+
+            var type = $(this).val();
+            var p = $('#descr-message');
+            var show = optObj[type]['display']['show'];
+            var noshow = optObj[type]['display']['noshow'];
+            
+            p.text(optObj[type]['txt']);
+            fst.find("label").text(optObj[type]['lbl1']);
+
+            $.each( show, function( key, val ) {
+                show.show();
+            });
+            $.each( noshow, function( key, val ) {
+                noshow.hide();
+            });
+
+        });
+
       });
 
 });
